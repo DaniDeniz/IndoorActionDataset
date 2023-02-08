8bb1bbc228acd40a900f8de3688929f0fb8c514c
DaniDeniz <dani.deniz@hotmail.com>
2023-02-08 16:04:05 +0000

    Include script to download dataset (no url).

---
+++ setup_video_data.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+RED='\033[0;31m'
+OFF='\033[0m'
+echo -e "${RED}Unable to download Indoor Action Dataset. \n${OFF}It will be available upon publication of the work where it is presented."
+exit 0
+
+
+echo "Start downloading video Indoor Action Dataset..."
+
+wget -O IndoorActionDataset-video.zip 
+
+echo "video Indoor Action Dataset download finished"
+
+echo "Unzip dataset into three folders: train, validation and test..."
+
+unzip ./IndoorActionDataset-video.zip
+
+echo "Unzip process finished"
