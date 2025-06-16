13d01496767191bdac6283cf16f80607b216f03e
Daniel Déniz <dani.deniz@hotmail.com>
2025-06-16 19:25:11 +0100

    Update setup_video_data.sh

--- setup_video_data.sh
+++ setup_video_data.sh
@@ -1,11 +1,5 @@
 #/bin/bash
 
-RED='\033[0;31m'
-OFF='\033[0m'
-echo -e "${RED}Unable to download Indoor Action Dataset. \n${OFF}It will be available upon publication of the work where it is presented."
-exit 0
-
-
 echo "Start downloading video Indoor Action Dataset..."
 
 wget -O IndoorActionDataset-video.zip https://drive.google.com/file/d/1YBJkc8czE0s76IbHbTfBvoS9pXaQnVXH/view?usp=sharing
